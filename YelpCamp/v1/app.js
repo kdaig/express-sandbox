a7956b8883995aec3561247ccd05b21ce8d364cb
Name <kyli.chick@gmail.com>
2019-04-19 23:01:40 +0000

    YCV1 added bootrap styling to hearder, campgrounds, and new. deleted info in footer

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 var campgrounds = [
         {name: "Catfish Highway", image: "https://images.unsplash.com/photo-1474139242267-eef6daa88e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80"},
         {name: "Muskrat Lake", image: "https://images.unsplash.com/photo-1554457686-c8409f3d8156?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1315&q=80"},
-        {name: "White Water", image: "https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"}
+        {name: "White Water", image: "https://stmedia.stimg.co/Whitewater+SP+1.jpg?auto=compress&crop=faces"}
         ]
 
 
