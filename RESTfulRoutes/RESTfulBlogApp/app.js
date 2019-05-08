07782a6739290bf4fe978641297c5316be02e68d
Name <kyli.chick@gmail.com>
2019-05-08 01:50:35 +0000

    comment goes here.

--- RESTfulRoutes/RESTfulBlogApp/app.js
+++ RESTfulRoutes/RESTfulBlogApp/app.js
@@ -4,7 +4,7 @@ var express = require("express");
 var app = express();
 
 //App config
-mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,18 +16,12 @@ var blogSchema = new mongoose.Schema({
     body: String,
     created:{type: Date, default: Date.now}
 });
+
 var Blog = mongoose.model("Blog", blogSchema);
 
 app.get("/", function(req, res) {
    res.redirect("/blogs"); 
 });
-
-//Blog Post for testing
-// Blog.create({
-//     title: "Test Blog",
-//     iamge: "https://images.unsplash.com/photo-1554791756-6d6cb6b45d5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-//     body: "Hello This is a blog post!"
-// });
 
 //Index Routes
 app.get("/blogs", function(req, res){
