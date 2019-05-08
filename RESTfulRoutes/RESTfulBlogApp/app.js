22ddbd2cd91d21e3ef5b2d3e8754763f2d6912a2
Name <kyli.chick@gmail.com>
2019-05-08 21:54:25 +0000

    Added Update Route.

--- RESTfulRoutes/RESTfulBlogApp/app.js
+++ RESTfulRoutes/RESTfulBlogApp/app.js
@@ -1,4 +1,5 @@
 var bodyParser  = require("body-parser");
+var methodOverride = require("method-override");
 var mongoose = require("mongoose");
 var express = require("express");
 var app = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //Mongoose/Model Config
 var blogSchema = new mongoose.Schema({
@@ -61,6 +63,27 @@ app.get("/blogs/:id", function(req, res) {
    }); 
 });
 
+//Edit Routes
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog:foundBlog});
+        }
+    });
+});
+
+//Update ROUTE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/", + req.params.id);
+        }
+    });
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
