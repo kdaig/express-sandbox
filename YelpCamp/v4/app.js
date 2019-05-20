0459e0b036ecff55d0f55632cfd33c50b9c45dd2
Name <kyli.chick@gmail.com>
2019-05-20 21:51:06 +0000

    worked on refactoring & comments

--- YelpCamp/v4/app.js
+++ YelpCamp/v4/app.js
@@ -3,19 +3,20 @@ var  express    = require("express"),
      bodyParser = require("body-parser"),
      mongoose   = require("mongoose"),
      Campground = require("./models/campground"),
+     Comment    = require("./models/comment"),
      seedDB     = require("./seeds");
 
 seedDB();
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v3", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/yelp_camp_v4", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 
-var campgrounds = [
-        {name: "Catfish Highway", image: "https://images.unsplash.com/photo-1474139242267-eef6daa88e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80"},
-        {name: "Muskrat Lake", image: "https://images.unsplash.com/photo-1554457686-c8409f3d8156?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1315&q=80"},
-        {name: "White Water", image: "https://stmedia.stimg.co/Whitewater+SP+1.jpg?auto=compress&crop=faces"}
-        ]
+// var campgrounds = [
+//         {name: "Catfish Highway", image: "https://images.unsplash.com/photo-1474139242267-eef6daa88e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80"},
+//         {name: "Muskrat Lake", image: "https://images.unsplash.com/photo-1554457686-c8409f3d8156?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1315&q=80"},
+//         {name: "White Water", image: "https://stmedia.stimg.co/Whitewater+SP+1.jpg?auto=compress&crop=faces"}
+//         ]
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -69,9 +70,33 @@ app.get("/campgrounds/:id", function(req, res) {
 //COMMENTS ROUTES
 //=====================
 app.get("/campgrounds/:id/comments/new", function(req, res) {
-    res.render("comments/new");
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("comments/new", {campground: campground});
+        }
+    })
 });
 
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err){
+           console.log(err);
+           res.redirect("/campgrounds"); 
+       } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            }); 
+       }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!!");
