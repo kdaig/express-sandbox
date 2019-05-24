fc966da86778cb0b89cee509406b51bcdf73c1a0
Name <kyli.chick@gmail.com>
2019-05-24 21:21:58 +0000

    added some middleware

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -8,7 +8,7 @@ var passportLocalMongoose = require("passport-local-mongoose");
 mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true });
 var app = express();
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "Favorite Film",
     resave: false,
@@ -18,21 +18,66 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-
+//=================
 //ROUTES
-
+//=================
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn , function(req, res){
     res.render("secret");
 });
+
+// Auth Routes
+app.get("/register", function(req, res) {
+   res.render("register"); 
+});
+//handling user sign up
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        })
+    });
+});
+
+// Log In ROutes
+//render login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+//loging logic
+app.post("/login",passport.authenticate("local", {
+   successRedirect: "/secret",
+   failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started.....");
