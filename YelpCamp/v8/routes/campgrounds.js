13c6391b8b52918644a4a9626afff847150d8bd1
Name <kyli.chick@gmail.com>
2019-05-31 21:54:06 +0000

    added v8 middleware to campgrounds

--- YelpCamp/v8/routes/campgrounds.js
+++ YelpCamp/v8/routes/campgrounds.js
@@ -19,7 +19,11 @@ router.post("/", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description:desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description:desc, author:author};
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
@@ -30,12 +34,12 @@ router.post("/", function(req, res){
 });
 
 //New show form form to create new campground
-router.get("/new", function(req,res){
+router.get("/new", isLoggedIn, function(req,res){
     res.render("campgrounds/new");
 });
 
 //Show 
-router.get("/:id", function(req, res) {
+router.get("/:id", isLoggedIn, function(req, res) {
    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
        if(err){
            console.log(err);
@@ -45,5 +49,12 @@ router.get("/:id", function(req, res) {
    });
    req.params.id
 });
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
 
 module.exports = router;
